74dac15f6a7327edebdc33ab2dd9ddd1eec69a9e
Axel <axel.paulander@gmail.com>
2018-07-10 17:21:02 +0200

    added matrix

---
+++ matrix/matrix.go
@@ -0,0 +1,37 @@
+// https://gist.github.com/n1try/c5082f0f1db7f4abcb6d995dc275fe7f
+/* Simple Matrix multiplication and transpostion with Go */
+package matrix
+
+import (
+	"fmt"
+	"errors"
+)
+
+type Matrix [][]float64
+
+func Transpose(x Matrix) Matrix {
+	out := make(Matrix, len(x[0]))
+	for i := 0; i < len(x); i += 1 {
+		for j := 0; j < len(x[0]); j += 1 {
+			out[j] = append(out[j], x[i][j])
+		}
+	}
+	return out
+}
+
+func Dot(x, y Matrix) (Matrix) {
+	if len(x[0]) != len(y) {
+    panic(errors.New("Can't do matrix multiplication."))
+	}
+
+	out := make(Matrix, len(x))
+	for i := 0; i < len(x); i += 1 {
+		for j := 0; j < len(y); j += 1 {
+			if len(out[i]) < 1 {
+				out[i] = make([]float64, len(y))
+			}
+			out[i][j] += x[i][j] * y[j][i]
+		}
+	}
+	return out, nil
+}
