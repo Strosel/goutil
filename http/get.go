35be59c8d878df9f6df52d0e46c33a5f3b8640d1
Axel <skullcandyapple@gmail.com>
2018-10-17 15:36:58 +0200

    Added basig get http Get parameters

---
+++ http/get.go
@@ -0,0 +1,27 @@
+package http
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func GetParamInt(r *http.Request, param string) int {
+	get, ok := r.URL.Query()["seed"]
+	if ok && len(get[0]) > 0 {
+		out, err := strconv.Atoi(string(get[0]))
+		if err != nil {
+			panic(err)
+		}
+		return out
+	}
+	return 0
+}
+
+func GetParamStr(r *http.Request, param string) string {
+	get, ok := r.URL.Query()["seed"]
+	if ok && len(get[0]) > 0 {
+		out := string(get[0])
+		return out
+	}
+	return ""
+}
