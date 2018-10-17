7204927553790a351dd964e3ed363b3e67d4dd27
Axel <skullcandyapple@gmail.com>
2018-10-17 15:49:50 +0200

    added float vs int vectors

--- Vector/Vector.go
+++ Vector/Vector.go
@@ -1,11 +1,22 @@
 package vector
 
 type Vector2 struct {
+	X float64
+	Y float64
+}
+
+type Vector2I struct {
 	X int
 	Y int
 }
 
 type Vector3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+type Vector3I struct {
 	X int
 	Y int
 	Z int
@@ -33,8 +44,38 @@ func (v1 Vector3) Add(v2 Vector3) Vector3 {
 	}
 }
 
-func (v1 Vector3) Add(v2 Vector3) Vector3 {
+func (v1 Vector3) Sub(v2 Vector3) Vector3 {
 	return Vector3{
+		X: v1.X - v2.X,
+		Y: v1.Y - v2.Y,
+		Z: v1.Z - v2.Z,
+	}
+}
+
+func (v1 Vector2I) Add(v2 Vector2I) Vector2I {
+	return Vector2I{
+		X: v1.X + v2.X,
+		Y: v1.Y + v2.Y,
+	}
+}
+
+func (v1 Vector2I) Sub(v2 Vector2I) Vector2I {
+	return Vector2I{
+		X: v1.X - v2.X,
+		Y: v1.Y - v2.Y,
+	}
+}
+
+func (v1 Vector3I) Add(v2 Vector3I) Vector3I {
+	return Vector3I{
+		X: v1.X + v2.X,
+		Y: v1.Y + v2.Y,
+		Z: v1.Z + v2.Z,
+	}
+}
+
+func (v1 Vector3I) Sub(v2 Vector3I) Vector3I {
+	return Vector3I{
 		X: v1.X - v2.X,
 		Y: v1.Y - v2.Y,
 		Z: v1.Z - v2.Z,
