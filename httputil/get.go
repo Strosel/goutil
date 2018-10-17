5838bc061c571749697c3cfe2dd62c590ca64588
Axel <skullcandyapple@gmail.com>
2018-10-17 16:04:16 +0200

    Fixed static parameter name

--- httputil/get.go
+++ httputil/get.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GetParamInt(r *http.Request, param string) (int, error) {
-	get, ok := r.URL.Query()["seed"]
+	get, ok := r.URL.Query()[param]
 	if ok && len(get[0]) > 0 {
 		out, err := strconv.Atoi(string(get[0]))
 		return out, err
@@ -16,7 +16,7 @@ func GetParamInt(r *http.Request, param string) (int, error) {
 }
 
 func GetParamStr(r *http.Request, param string) string {
-	get, ok := r.URL.Query()["seed"]
+	get, ok := r.URL.Query()[param]
 	if ok && len(get[0]) > 0 {
 		out := string(get[0])
 		return out
